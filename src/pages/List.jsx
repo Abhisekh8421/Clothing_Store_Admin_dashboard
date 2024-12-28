5a13f90e462c9cf83bad91c436b2bd313348e8af
Suru Abhisekh <abhisekhsuru@gmail.com>
2024-12-28 22:56:01 +0530

    working on the Admin Dashboard

---
+++ src/pages/List.jsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from "react";
+import { backendUrl } from "../App";
+import axios from "axios";
+import { toast } from "react-toastify";
+
+const List = ({ token }) => {
+  const [list, setList] = useState([]);
+
+  const fetchList = async () => {
+    try {
+      const response = await axios.get(backendUrl + "/api/product/list", {
+        headers: { token },
+      });
+
+      if (response.data.success) {
+        setList(response.data.products);
+      } else {
+        toast.error(response.data.message);
+      }
+    } catch (error) {
+      console.log(error.message);
+      toast.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchList();
+  }, []);
+
+  return (
+    <>
+      <p className="mb-2">All Products List</p>
+      <div>
+        
+      </div>
+    </>
+  );
+};
+
+export default List;
