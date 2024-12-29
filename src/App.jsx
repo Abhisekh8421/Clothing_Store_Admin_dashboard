eb0db3c689fb2a7683af32ea022ee66632a06f0e
Suru Abhisekh <abhisekhsuru@gmail.com>
2024-12-29 12:45:26 +0530

    added the functionality of Add items and List Items by Admin

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Login from "./components/Login";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
 export const backendUrl = import.meta.env.VITE_BACKEND_URL;
+export const currency = "₹";
 
 const App = () => {
   const [token, setToken] = useState(
